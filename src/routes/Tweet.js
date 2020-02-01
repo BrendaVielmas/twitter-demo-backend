bf5b4e00d80a7cf6ce00b354fe8477769170ea3e
Brenda Vielmas <breen.vielmas@gmail.com>
2020-02-01 13:05:53 -0600

    heroku deploy

--- src/routes/Tweet.js
+++ src/routes/Tweet.js
@@ -1,4 +1,15 @@
-const T = require('../Key.js')
+
+let Twit = require('twit')
+
+let T = new Twit({
+  consumer_key:         process.env.COSTUMERKEY,
+  consumer_secret:      process.env.COSTUMERSECRET,
+  access_token:         process.env.ACCESSTOKEN,
+  access_token_secret:  process.env.ACCESSTOKENSECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
 module.exports.getTweetsByScreenName = (req, res) => {
   let screenName = req.params.screenName;
   T.get('statuses/user_timeline', { screen_name: screenName, count: 10 }, (err, data, response) => {
